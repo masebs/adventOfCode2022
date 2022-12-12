f49480bd2ebb89bff1d4e38817ab048d98d93f16
mss2304 <75536648+mss2304@users.noreply.github.com>
2022-12-12 11:46:19 +0100

    day 12 faster (using BFS instead of Dijkstra)

--- day12.py
+++ day12.py
@@ -27,15 +27,6 @@ class Node:
         self.dist  = 9e9
         self.prev  = None
 
-def getNeighborIndices(n, bounds):
-    nbs = []
-    i,j = n.index
-    idcs = [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
-    for idx in idcs:
-        if 0 <= idx[0] < bounds[0] and 0 <= idx[1] < bounds[1]:
-            nbs.append(idx)
-    return nbs
-
 def createGraph(grid):
     ngrid = []
     for i, r in enumerate(grid):
@@ -45,7 +36,18 @@ def createGraph(grid):
             ngridrow.append(n)
         ngrid.append(ngridrow)
     return ngrid
-        
+
+def getNeighborIndices(n, bounds):
+    nbs = []
+    i,j = n.index
+    idcs = [(i-1, j), (i, j-1), (i+1, j), (i, j+1)]
+    for idx in idcs:
+        if 0 <= idx[0] < bounds[0] and 0 <= idx[1] < bounds[1]:
+            nbs.append(idx)
+    return nbs
+
+# Breadth-first search is sufficient here, with result = depth at which we first found the target,
+# because the graph is unweighted and the node values are only used to determine whether an edge exists
 def BFS(grid, startidx, endidx, backwards=False):
     q = deque()
     visited = set()
@@ -58,24 +60,30 @@ def BFS(grid, startidx, endidx, backwards=False):
     visited.add(start.index)
     
     while q:
-        print(len(visited))
         n = q.popleft()
+        
         if (not backwards and n == end) or (backwards and n.value == 0):
             return n
         
         nbs = [ngrid[idx[0]][idx[1]] for idx in getNeighborIndices(n, (len(ngrid), len(ngrid[0])))]
         for nbr in nbs:
             if nbr.index not in visited:
-                visited.add(nbr.index)
+                # determine whether an edge exists (only for values rising no more than 1, or falling if going backwards)
                 if (not backwards and nbr.value-n.value <= 1) or (backwards and nbr.value-n.value >= -1):
                     nbr.prev = n
                     nbr.dist = n.dist + 1
                     q.append(nbr)
-                
+                    visited.add(nbr.index)
+    return None
+ 
+# Adapted Dijkstra from last year, using the node values to determine whether an edge exists,
+# but treating the graph as unweighted (just adding 1 for each level of depth)
+# Actually slower than BFS because of the overhead of the priority queue, 
+# which doesn't bring any benefit in an unweighted graph (with deque instead of prioQ: very similar to BFS)                
 def dijkstra(grid, startidx, endidx, backwards=False): # with distance = number of steps
-    yellow = PriorityQueue() # prio q seems to reduce by a factor of almost 10 to ~3s
-    yellowSet = set() # using additional Set for more efficient "in" (contains) op saves ~3s
-    green  = set() # using set instead of list for green: 26 s w/o, 45 s with output instead of > 10 min with list
+    yellow = PriorityQueue()
+    yellowSet = set() 
+    green  = set() 
     ngrid = createGraph(grid)
     
     start = ngrid[startidx[0]][startidx[1]]
@@ -109,6 +117,7 @@ def dijkstra(grid, startidx, endidx, backwards=False): # with distance = number 
                         nbr.prev = n
                         # nbr.dist = n.dist + nbr.value # this would be usual Dijkstra (looking for total cost)
                         nbr.dist = n.dist + 1 # here: distance = path length, so just increment
+    return None
 
 def traceback(end, startidx): # for debug or visualization
     # trace back to start
@@ -121,21 +130,21 @@ def traceback(end, startidx): # for debug or visualization
         print(f"({n.index[1]}, {n.index[0]}): {chr(n.value+ord('a'))} {n.dist}")
         countsteps += 1
     
-def main():
-    grid, startidx, endidx = readInput("input-day12")
-    # grid, startidx, endidx = readInput("input-day12-test")
-    
-    # Task 1
-    endnode = dijkstra(grid, startidx, endidx)
-    # endnode = BFS(grid, startidx, endidx)
-    # traceback(endnode, startidx)
-    print(f"Task 1: Easiest path takes {endnode.dist} steps")
-    
-    # Task 2: search from target to any point with height 'a' 
-    # (this works because the first one found is automatically the one with the shortest path, as we only do BFS)
-    endnode = dijkstra(grid, endidx, None, backwards=True) 
-    # endnode = BFS(grid, endidx, None, backwards=True) 
-    print(f"Task 2: Fewest steps from any possible starting point is {endnode.dist}")
+# def main():
+grid, startidx, endidx = readInput("input-day12")
+# grid, startidx, endidx = readInput("input-day12-test")
 
-if __name__ == "__main__":
-    main()
+# Task 1
+# endnode = dijkstra(grid, startidx, endidx)
+endnode = BFS(grid, startidx, endidx)
+# traceback(endnode, startidx)
+print(f"Task 1: Easiest path takes {endnode.dist} steps")
+
+# # Task 2: search from target to any point with height 'a' 
+# # (this works because the first one found is automatically the one with the shortest path, as we only do BFS)
+# endnode = dijkstra(grid, endidx, None, backwards=True) 
+endnode = BFS(grid, endidx, None, backwards=True) 
+print(f"Task 2: Fewest steps from any possible starting point is {endnode.dist}")
+
+# if __name__ == "__main__":
+#     main()
